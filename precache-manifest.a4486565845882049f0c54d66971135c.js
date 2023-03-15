84f88ec7837c5a00f0a1272d709a748b47d2d42d
DerpyTheSpy <erickim334@gmail.com>
2023-03-15 16:38:35 -0400

    Updates

---
+++ precache-manifest.a4486565845882049f0c54d66971135c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "503ee48e7cd1aefe65c2",
+    "url": "/portfolio/static/css/main.b0f07d6d.chunk.css"
+  },
+  {
+    "revision": "503ee48e7cd1aefe65c2",
+    "url": "/portfolio/static/js/main.503ee48e.chunk.js"
+  },
+  {
+    "revision": "02feadb2adb76b10ee9b",
+    "url": "/portfolio/static/js/1.02feadb2.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "f29fdbd128aab56f69fee2fa44a45781",
+    "url": "/portfolio/index.html"
+  }
+];
